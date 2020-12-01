6338f9db47def723470a74056470a83cfdc3e517
nxtfsm <sysadmin@xaviermicronesia.org>
2020-12-01 15:10:49 -0600

    init express middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // ./index.js
 import 'dotenv/config';
-import Server from './src';
+import Server, { launchExpress } from './src';
 
-Server({
+const config = {
   port: process.env.PORT,
   remoteURI: process.env.MONGODB_URI,
   debugging: process.env.NODE_ENV === 'debugging' ? true : null,
-});
+};
+
+const app = launchExpress(config.port);
+
+Server(app, config);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 // ./src/index.js
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import schema from './schema';
 import databaseClient from './databaseClient';
 import dataSources from './dataSources';
 import logger from './__debugger__';
 
-export default async function(config) {
+export default async function(app, config) {
   const { port, remoteURI, debugging } = config;
+  if (!app) process.exit();
+
   const server = new ApolloServer({
     ...schema,
     dataSources: () => dataSources(databaseClient.getDB()),
   });
 
-  server.listen({ port })
-    .then(({url}) => {
-      databaseClient.connect(remoteURI)
-        .then((res, rej) => {
-          const activeMsg = `🖖 Server ready at ${url}`;
-          debugging ? logger({info: res}) : logger();
-          logger(activeMsg);
-        })
-        .catch((rej) => {
-          logger({err: rej});
-          process.exit();
-        });
-    });
+  server.applyMiddleware({ app });
+
+  app.listen({ port }, () => {
+    databaseClient.connect(remoteURI)
+      .then((res, rej) => {
+        const activeMsg = `🖖 Server ready at http://localhost:${port}`;
+        debugging ? logger({info: res}) : logger();
+        logger(activeMsg);
+      })
+      .catch((rej) => {
+        logger({err: rej});
+        process.exit();
+      });
+  });
+}
+
+export function launchExpress(port) {
+  let status = null;
+  try {
+    const app = express();
+    status = app;
+  } catch {
+    logger({err: 'express error!'});
+  } finally {
+    return status;
+  }
 }
