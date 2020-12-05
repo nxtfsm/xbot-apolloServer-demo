da09748390b53d15ebcd004a08b3612210207ae2
nxtfsm <sysadmin@xaviermicronesia.org>
2020-12-05 16:26:31 -0600

    Shorten query method

--- src/schema/resolverConstructor/_queryConstructor.js
+++ src/schema/resolverConstructor/_queryConstructor.js
@@ -1,17 +1,15 @@
 // ./src/schema/resolverConstructor/_queryConstructor.js
 import inputReducer from './_inputReducer';
 
-export default tutorialsQuery;
+export default query;
 
-function tutorialsQuery({
-  articleQuery, pageSize = 10, after = 0,
-}, inCollection) {
+function query({ articleQuery, pageSize = 10, after = 0, }, collection) {
   return {
     async getTutorials() {
       const args = inputReducer(articleQuery);
       const opts = { pageSize, after };
       const newCursor = pageSize + after - 1;
-      const results = await inCollection.getAll(args, opts);
+      const results = await collection.getAll(args, opts);
 
       results.hasMore = !!(newCursor < results.estCount);
       results.cursor = results.hasMore ? newCursor : results.estCount;
@@ -19,4 +17,4 @@ function tutorialsQuery({
       return results;
     },
   };
-}
+};
